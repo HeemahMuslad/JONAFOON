d98dde2468894239201b9981c61355b014b4570d
Rohimat Mustapha <rohimat.m1800276@st.futminna.edu.ng>
2023-12-13 09:38:15 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navlink = document.querySelector(".navLinks");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navlink.classList.toggle("active");
+});
+document.querySelectorAll(".link").forEach((link) =>
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navlink.classList.remove("active");
+  })
+);
